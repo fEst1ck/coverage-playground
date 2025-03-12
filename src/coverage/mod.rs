0979bf17a9022975ce259f952dfbcf757c776e39
Zekun Wang <wswzk864864@gmail.com>
2025-03-12 06:07:31 -0400

    remove coverage 'all'

--- src/coverage/mod.rs
+++ src/coverage/mod.rs
@@ -1,40 +1,12 @@
-mod all;
 mod block;
 mod edge;
 mod path;
 use std::collections::BTreeMap;
 
-use all::AllCoverage;
 pub use block::BlockCoverage;
 pub use edge::EdgeCoverage;
 pub use path::PathCoverage;
 use serde_json::Value;
-
-#[derive(Debug, Clone, Copy)]
-pub enum CoverageType {
-    Block,
-    Edge,
-    Path,
-}
-
-impl Default for CoverageType {
-    fn default() -> Self {
-        CoverageType::Edge
-    }
-}
-
-impl std::str::FromStr for CoverageType {
-    type Err = String;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s.to_lowercase().as_str() {
-            "block" => Ok(CoverageType::Block),
-            "edge" => Ok(CoverageType::Edge),
-            "path" => Ok(CoverageType::Path),
-            _ => Err(format!("Unknown coverage type: {}", s)),
-        }
-    }
-}
 
 pub trait CoverageMetric {
     /// Update coverage with the given path and return true if new coverage was found
@@ -46,21 +18,6 @@ pub trait CoverageMetric {
     /// Get the name of the metric
     fn name(&self) -> &str {
         ""
-    }
-}
-
-pub fn create_coverage_metric(
-    coverage_type: CoverageType,
-    all_coverage: bool,
-) -> Box<dyn CoverageMetric> {
-    if all_coverage {
-        Box::new(AllCoverage::new(coverage_type))
-    } else {
-        match coverage_type {
-            CoverageType::Block => Box::new(BlockCoverage::default()),
-            CoverageType::Edge => Box::new(EdgeCoverage::default()),
-            CoverageType::Path => Box::new(PathCoverage::default()),
-        }
     }
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,4 @@ pub mod coverage;
 pub mod fuzzer;
 
 pub use cli::Args;
-pub use coverage::CoverageType;
 pub use fuzzer::{Fuzzer, FuzzerError, Result};
