d0a8fca9818c8b2499c738701d255a026793f10b
Zekun Wang <zekun.wang@uwaterloo.ca>
2025-08-27 01:34:56 -0400

    opt

--- src/coverage/quad.rs
+++ src/coverage/quad.rs
@@ -4,10 +4,41 @@ use super::{CoverageFeedback, CoverageMetric};
 use rustc_hash::{FxHashMap, FxHashSet};
 use serde_json::Value;
 
-#[derive(Default)]
+use path_reduction::json_parser::parse_json_file;
+
+type BlockID = u32;
+
 pub struct QuadCoverage {
     edges: FxHashMap<(u32, u32), usize>,
     raw_edge: EdgeCoverage,
+    first_to_lasts: FxHashMap<BlockID, FxHashSet<BlockID>>,
+}
+
+impl QuadCoverage {
+    pub fn from_json(path: &str) -> Self {
+        let modules = parse_json_file(path).unwrap();
+        let first_to_lasts = modules
+            .iter()
+            .flat_map(|module| {
+                module
+                    .functions
+                    .iter()
+                    .map(|func| (func.entry_block, func.exit_blocks.iter().cloned().collect()))
+            })
+            .collect();
+        Self {
+            edges: Default::default(),
+            raw_edge: EdgeCoverage::default(),
+            first_to_lasts,
+        }
+    }
+}
+
+impl Default for QuadCoverage {
+    fn default() -> Self {
+        let cfg_file = std::env::var("CFG_FILE").unwrap_or_default();
+        Self::from_json(&cfg_file)
+    }
 }
 
 impl CoverageMetric for QuadCoverage {
@@ -20,6 +51,9 @@ impl CoverageMetric for QuadCoverage {
         let mut prev_blocks: FxHashSet<u32> = FxHashSet::default();
 
         for block in path {
+            if !self.first_to_lasts.contains_key(&block) {
+                continue;
+            }
             for prev_block in &prev_blocks {
                 let edge = (*prev_block, *block);
                 let count = *self.edges.entry(edge).and_modify(|count| *count += 1).or_insert_with(|| {
