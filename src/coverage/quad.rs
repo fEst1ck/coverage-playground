2c3a10aeb358120d8949a549010b1931e6c8bf32
Zekun Wang <zekun.wang@uwaterloo.ca>
2025-08-28 02:10:13 -0400

    update quad metric to log n distances

--- src/coverage/quad.rs
+++ src/coverage/quad.rs
@@ -1,68 +1,42 @@
 use crate::coverage::EdgeCoverage;
 
 use super::{CoverageFeedback, CoverageMetric};
-use rustc_hash::{FxHashMap, FxHashSet};
+use rustc_hash::FxHashMap;
 use serde_json::Value;
 
-use path_reduction::json_parser::parse_json_file;
-
-type BlockID = u32;
-
+#[derive(Default)]
 pub struct QuadCoverage {
     edges: FxHashMap<(u32, u32), usize>,
     raw_edge: EdgeCoverage,
-    first_to_lasts: FxHashMap<BlockID, FxHashSet<BlockID>>,
-}
-
-impl QuadCoverage {
-    pub fn from_json(path: &str) -> Self {
-        let modules = parse_json_file(path).unwrap();
-        let first_to_lasts = modules
-            .iter()
-            .flat_map(|module| {
-                module
-                    .functions
-                    .iter()
-                    .map(|func| (func.entry_block, func.exit_blocks.iter().cloned().collect()))
-            })
-            .collect();
-        Self {
-            edges: Default::default(),
-            raw_edge: EdgeCoverage::default(),
-            first_to_lasts,
-        }
-    }
-}
-
-impl Default for QuadCoverage {
-    fn default() -> Self {
-        let cfg_file = std::env::var("CFG_FILE").unwrap_or_default();
-        Self::from_json(&cfg_file)
-    }
 }
 
 impl CoverageMetric for QuadCoverage {
     fn update_from_path(&mut self, path: &[u32]) -> CoverageFeedback {
+        let mut n = path.len();
+        let mut logs = vec![];
+        while n >= 4 {
+            logs.push(n / 2);
+            n /= 2;
+        }
         let raw_edge_feedback = self.raw_edge.update_from_path(path);
 
         let mut new_coverage = false;
 
         let mut uniq = usize::MAX;
-        let mut prev_blocks: FxHashSet<u32> = FxHashSet::default();
 
-        for block in path {
-            if !self.first_to_lasts.contains_key(&block) {
-                continue;
-            }
-            for prev_block in &prev_blocks {
-                let edge = (*prev_block, *block);
-                let count = *self.edges.entry(edge).and_modify(|count| *count += 1).or_insert_with(|| {
+        for (i, block) in path.iter().enumerate() {
+            for &distance in &logs {
+                let j = i + distance;
+                if j < path.len() {
+                    let succ = path[j];
+                    let edge = (*block, succ);
+                    let count = *self.edges.entry(edge).and_modify(|count| *count += 1).or_insert_with(|| {
                     new_coverage = true;
                     1
                 });
                 uniq = uniq.min(count);
+                }
             }
-            prev_blocks.insert(*block);
         }
 
         if raw_edge_feedback.new_cov() {
