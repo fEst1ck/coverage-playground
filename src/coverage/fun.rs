84fc88a8c299ec81c3ae45789d1acac04b65728f
Zekun Wang <zekun.wang@uwaterloo.ca>
2025-12-23 01:38:10 -0500

    add missing file

---
+++ src/coverage/fun.rs
@@ -0,0 +1,82 @@
+use super::{CoverageFeedback, CoverageMetric};
+use path_reduction::json_parser::parse_json_file;
+use rustc_hash::FxHashMap;
+use serde_json::Value;
+
+pub struct FunCoverage {
+    funs: FxHashMap<u32, usize>,
+    first_map: FxHashMap<u32, u32>,
+}
+
+impl Default for FunCoverage {
+    fn default() -> Self {
+        let cfg_file = std::env::var("CFG_FILE").unwrap_or_default();
+        Self::from_json(&cfg_file)
+    }
+}
+
+impl FunCoverage {
+    fn from_json(path: &str) -> Self {
+        let modules = parse_json_file(path).unwrap();
+        let first_to_lasts = modules
+            .iter()
+            .flat_map(|module| {
+                module
+                    .functions
+                    .iter()
+                    .map(|func| (func.entry_block, func.exit_blocks.iter().cloned().collect()))
+            })
+            .collect();
+        Self {
+            funs: FxHashMap::default(),
+            first_map: first_to_lasts,
+        }
+    }
+}
+
+impl CoverageMetric for FunCoverage {
+    fn update_from_path(&mut self, path: &[u32]) -> CoverageFeedback {
+        let mut new_cov = false;
+        let mut uniq = usize::MAX;
+        for block in path {
+            if self.first_map.contains_key(block) {
+                let count = *self
+                    .funs
+                    .entry(*block)
+                    .and_modify(|count| *count += 1)
+                    .or_insert_with(|| {
+                        new_cov = true;
+                        1
+                    });
+                uniq = uniq.min(count);
+            }
+        }
+        if new_cov {
+            CoverageFeedback::NewBlock { uniqueness: uniq }
+        } else {
+            CoverageFeedback::NoCoverage(uniq)
+        }
+    }
+
+    fn cov_info(&self) -> Value {
+        Value::Number(self.funs.len().into())
+    }
+
+    // an array of [from, to, count]
+    fn full_cov(&self) -> Value {
+        Value::Array(
+            self.funs
+                .iter()
+                .map(|(fun_id, _)| Value::Number((*fun_id).into()))
+                .collect(),
+        )
+    }
+
+    fn name(&self) -> &'static str {
+        "fun"
+    }
+
+    fn priority(&self) -> usize {
+        90
+    }
+}
